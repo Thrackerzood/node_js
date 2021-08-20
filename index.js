c05d36548ce3443d8177d85eecaa941b4fc8fc09
Thrackerzood <71223383+Thrackerzood@users.noreply.github.com>
2021-08-20 22:00:32 +0400

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-const cors = require('cors')
-const corsOptions = {
-    origin: 'https://sql-basedata.herokuapp.com/',
-    optionsSuccessStatus: 200
-}
-// problems
-app.options('*', cors(corsOptions))
-app.use(cors(corsOptions))
+
 
 
 
